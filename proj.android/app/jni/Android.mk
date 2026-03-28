9e2b685ad632b33425ff0c76c4db3b85886c8502
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-03-28 13:40:57 +0800

    build: Add ObstacleView to build configuration and update Android setup
    - Add ObstacleView.cpp and ObstacleView.h to CMakeLists.txt source and header lists
    - Update Android application ID from "my-tennis-balls" to "mytennisballs" for consistency
    - Expand Android.mk with complete source file list including all scene, model, view, and controller classes
    - Add comprehensive LOCAL_C_INCLUDES paths for organized header file resolution across all module directories
    - Consolidate build configuration to properly reference reorganized project structure

--- proj.android/app/jni/Android.mk
+++ proj.android/app/jni/Android.mk
@@ -8,9 +8,28 @@ LOCAL_MODULE_FILENAME := libMyGame
 
 LOCAL_SRC_FILES := $(LOCAL_PATH)/hellocpp/main.cpp \
                    $(LOCAL_PATH)/../../../Classes/AppDelegate.cpp \
-                   $(LOCAL_PATH)/../../../Classes/GameScene.cpp
+                   $(LOCAL_PATH)/../../../Classes/scene/GameScene.cpp \
+                   $(LOCAL_PATH)/../../../Classes/scene/LevelMenuScene.cpp \
+                   $(LOCAL_PATH)/../../../Classes/model/GameModel.cpp \
+                   $(LOCAL_PATH)/../../../Classes/model/LevelData.cpp \
+                   $(LOCAL_PATH)/../../../Classes/model/ScoreManager.cpp \
+                   $(LOCAL_PATH)/../../../Classes/view/AimLineView.cpp \
+                   $(LOCAL_PATH)/../../../Classes/view/ArenaView.cpp \
+                   $(LOCAL_PATH)/../../../Classes/view/BallView.cpp \
+                   $(LOCAL_PATH)/../../../Classes/view/HUD.cpp \
+                   $(LOCAL_PATH)/../../../Classes/view/TrayView.cpp \
+                   $(LOCAL_PATH)/../../../Classes/view/VFXHelper.cpp \
+                   $(LOCAL_PATH)/../../../Classes/view/AmbientParticles.cpp \
+                   $(LOCAL_PATH)/../../../Classes/view/ObstacleView.cpp \
+                   $(LOCAL_PATH)/../../../Classes/controller/InputController.cpp \
+                   $(LOCAL_PATH)/../../../Classes/controller/GameController.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
+                    $(LOCAL_PATH)/../../../Classes/common \
+                    $(LOCAL_PATH)/../../../Classes/scene \
+                    $(LOCAL_PATH)/../../../Classes/model \
+                    $(LOCAL_PATH)/../../../Classes/view \
+                    $(LOCAL_PATH)/../../../Classes/controller
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
